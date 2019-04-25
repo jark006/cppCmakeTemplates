e53de33e4a2db54494b00ee2ca32dc3edf3c2908
jark006 <jark006@qq.com>
2019-04-25 20:49:26 +0800

    add cpp basic

---
+++ cppBasic/src/add.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+
+int add(int input)
+{
+	int i = input;
+	input = i+i;
+	return input;
+}
---
+++ cppBasic/src/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "add.h"
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int i;
+	cin >> i;
+	i = add(i);
+	cout << i << endl;
+
+}
